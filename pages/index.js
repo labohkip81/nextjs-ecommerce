5cc18f334332a0932af70a776542b4d73d31bec9
labohkip81 <labankiplagat81@gmail.com>
2022-04-25 13:18:27 +0300

    Add styling of the components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const index = () => {
+function Home() {
   return (
-    <div>index</div>
+    <>
+    HeroBanner
+
+    <div>
+      <h2> Best Selling Products</h2>
+      <p>Speakers of many variations</p>
+      
+    </div>
+
+    <div>
+      {["Product 1", "Product 2", "Product 3", "Product 4", "Product 5"].map((product) => product)}
+    </div>
+
+    Footer
+    </>
   )
 }
 
-export default index
+export default Home
