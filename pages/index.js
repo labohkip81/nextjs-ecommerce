f6faf2b6efaf3be18e3a6b57ef2bb155a94cf80f
labohkip81 <labankiplagat81@gmail.com>
2022-04-25 15:03:59 +0300

    add components and sanity setup

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { FooterBanner, HeroBanner } from '../components';
 
 function Home() {
   return (
     <>
-    HeroBanner
+    <HeroBanner />
 
-    <div>
+    <div className='products-heading'>
       <h2> Best Selling Products</h2>
       <p>Speakers of many variations</p>
-      
+
     </div>
 
-    <div>
+    <div className='products-container'>
       {["Product 1", "Product 2", "Product 3", "Product 4", "Product 5"].map((product) => product)}
     </div>
 
-    Footer
+    <FooterBanner />
     </>
   )
 }
